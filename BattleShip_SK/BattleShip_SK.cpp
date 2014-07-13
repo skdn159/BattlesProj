303b71bea61d29d1a837b4133e8cce9e7e578ff5
skdn159 <skdn159@naver.com>
2014-07-14 03:21:26 +0900

    reg code

---
+++ BattleShip_SK/BattleShip_SK.cpp
@@ -0,0 +1,28 @@
+// BattleShip_SK.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	srand((unsigned int)time(NULL)); //time을 이용한 seed값 생성
+
+	std::string strInput;
+	std::cin >> strInput;
+
+	std::string strResult[] = { "Hit", "MISS", "DESTROY" };
+
+
+
+	int result = rand() % _countof(strResult);
+	std::cout << result << std::endl;
+
+	std::cout << strResult[(result)];
+
+	fflush(stdin);
+	getchar();
+
+
+	return 0;
+}
